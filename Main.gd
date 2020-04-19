c8a1606d2f0ec1e38b8db347e125389bffef6b35
Jayshua Nelson <me@jayshuanelson.com>
2020-04-18 21:59:47 -0500

    More Work

--- Main.gd
+++ Main.gd
@@ -2,10 +2,11 @@ extends Node2D
 
 onready var player = $Player
 onready var ship = $Ship
+onready var shipConnectionDetector = $"Ship/ConnectionDetector"
 
 func _process(delta):
 	if player.player_oxygen <= 0:
 		get_tree().change_scene("res://scenes/GameOver.tscn")
 	
-	#if ship.oxygen <= 0 && ship.get_indexed("ConnectionDetector").overlaps_body(player):
-	#	get_tree().change_scene("res://scenes/GameOver.tscn")
+	if ship.oxygen <= 0 && shipConnectionDetector.overlaps_body(player):
+		get_tree().change_scene("res://scenes/GameOver.tscn")
