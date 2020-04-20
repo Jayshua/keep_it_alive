736413f77bf83821a26eec85b292d428cbc19f57
ConnerEnders <conner.enders@icloud.com>
2020-04-19 21:31:19 -0500

    Angler stuff, please help me D:

--- WorldEnvironment.gd
+++ WorldEnvironment.gd
@@ -2,9 +2,9 @@ extends WorldEnvironment
 
 onready var player = $"../Player"
 
-func _process(delta):
+func _process(_delta):
 	var depth = player.position.y
-	self.environment.glow_intensity = lerp(0, 1.2, clamp(depth / 500, 0, 1))
-	self.environment.glow_bloom = lerp(0, 1.2, clamp(depth / 500, 0, 1))
-	self.environment.adjustment_brightness = lerp(1, 0.7, clamp(depth / 1000, 0, 1))
-	self.environment.adjustment_contrast = lerp(1, 1.5, clamp(depth / 1000, 0, 1))
+	self.environment.glow_intensity = lerp(0, 1.4, clamp(depth / 500, 0, 1))
+	self.environment.glow_bloom = lerp(0, 1.4, clamp(depth / 500, 0, 1))
+	self.environment.adjustment_brightness = lerp(1, 0.4, clamp(depth / 1000, 0, 1))
+	self.environment.adjustment_contrast = lerp(1, 3, clamp(depth / 1000, 0, 1))
