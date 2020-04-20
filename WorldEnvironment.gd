c2b0b24eba6f01267ccc90064d34593be6b27a25
ConnerEnders <conner.enders@icloud.com>
2020-04-20 13:26:23 -0500

    Adjusting light and angler stuff

--- WorldEnvironment.gd
+++ WorldEnvironment.gd
@@ -4,7 +4,7 @@ onready var player = $"../Player"
 
 func _process(_delta):
 	var depth = player.position.y
-	self.environment.glow_intensity = lerp(0, 1.4, clamp(depth / 500, 0, 1))
-	self.environment.glow_bloom = lerp(0, 1.4, clamp(depth / 500, 0, 1))
-	self.environment.adjustment_brightness = lerp(1, 0.4, clamp(depth / 1000, 0, 1))
-	self.environment.adjustment_contrast = lerp(1, 3, clamp(depth / 1000, 0, 1))
+	self.environment.glow_intensity = lerp(0.5, 2, clamp(depth / 500, 0, 1))
+	self.environment.glow_bloom = lerp(0.5, 2, clamp(depth / 500, 0, 1))
+	self.environment.adjustment_brightness = lerp(1, 0.8, clamp(depth / 1000, 0, 1))
+	self.environment.adjustment_contrast = lerp(1, 2, clamp(depth / 1000, 0, 1))
