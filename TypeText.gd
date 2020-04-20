1630be8846f523d42d1ad9a4d18d731594af5e9f
Jayshua Nelson <me@jayshuanelson.com>
2020-04-19 23:49:30 -0500

    More work

--- TypeText.gd
+++ TypeText.gd
@@ -4,5 +4,8 @@ onready var start = OS.get_ticks_msec()
 
 func _process(delta):
 	var ellapsed = OS.get_ticks_msec() - start
-	self.visible_characters = int(float(ellapsed) / 20)
+	self.visible_characters = int(float(ellapsed) / 10)
+
+	if self.percent_visible < 1:
+		$AudioStreamPlayer.play()
 
